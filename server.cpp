1826e8299f4e2eb37dac807786ea390e90c3fb6e
chanhpar <chanhpar@student.42seoul.kr>
2023-01-24 19:33:12 +0900

    WIP: multiclient with select, nonblock

--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <stdio.h>
@@ -6,6 +7,7 @@
 #include <string.h>
 #include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -21,6 +23,11 @@ main(void) {
 
   struct sockaddr client_info;
   socklen_t client_info_len = sizeof(client_info);
+
+  struct timeval timeout;
+  fd_set master_set, working_set;
+
+  bool end_flag = false;
 
 #if defined(__APPLE__)
   int sfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -49,36 +56,132 @@ main(void) {
     exit(EXIT_FAILURE);
   }
 
-  int cfd = accept(sfd, &client_info, &client_info_len);
-  if (cfd < 0) {
-    perror("accept");
-    close(sfd);
-    exit(EXIT_FAILURE);
-  }
+  FD_ZERO(&master_set);
+  int max_sfd = sfd;
+  FD_SET(sfd, &master_set);
+
+  timeout.tv_sec = 3 * 60;
+  timeout.tv_usec = 0;
 
   char buffer[BUFFER_SIZE];
 
-  while (1) {
-    memset(buffer, 0, BUFFER_SIZE);
+  do {
+    memcpy(&working_set, &master_set, sizeof(master_set));
+    printf("wait for select\n");
+    int ready_sfd_count
+        = select(max_sfd + 1, &working_set, NULL, NULL, &timeout);
 
-    ssize_t recv_len = recv(cfd, buffer, BUFFER_SIZE - 1, 0);
-    if (recv_len < 0) {
-      perror("recv");
+    if (ready_sfd_count < 0) {
+      perror("select");
       break;
     }
-    printf("recieved: %s\n", buffer);
 
-    size_t buffer_len = strlen(buffer);
-    buffer[buffer_len] = '$';
-    buffer[buffer_len + 1] = '\0';
-
-    ssize_t send_len = send(cfd, buffer, buffer_len + 1, 0);
-    if (send_len < 0) {
-      perror("send");
+    if (ready_sfd_count == 0) {
+      printf("select timeout\n");
       break;
     }
+
+    for (int i = 0; i < max_sfd && ready_sfd_count > 0; ++i) {
+      if (FD_ISSET(i, &working_set)) {
+        ready_sfd_count -= 1;
+        if (i == sfd) {
+          printf("readable listening socket\n");
+          int cfd;
+          while (1) {
+            cfd = accept(sfd, &client_info, &client_info_len);
+            if (cfd < 0) {
+              if (errno != EWOULDBLOCK) {
+                perror("accept");
+                end_flag = true;
+              }
+              break;
+            }
+
+            printf("new connection: %d\n", cfd);
+            FD_SET(cfd, &master_set);
+            if (cfd > max_sfd)
+              max_sfd = cfd;
+          }
+        } else {
+          printf("readable other socket %d\n", i);
+
+          bool close_connection = false;
+
+          while (1) {
+            ssize_t recv_len = recv(i, buffer, BUFFER_SIZE - 1, 0);
+            // XXX
+
+            if (recv_len < 0) {
+              if (errno != EWOULDBLOCK) {
+                perror("recv");
+                close_connection = true;
+              }
+              break;
+            }
+
+            if (recv_len == 0) {
+              printf("connection closed\n");
+              close_connection = true;
+              break;
+            }
+
+            printf("recieved %ld bytes\n", recv_len);
+            size_t buffer_len = strlen(buffer);
+            buffer[buffer_len] = '$';
+            buffer[buffer_len + 1] = '\0';
+
+            ssize_t send_len = send(i, buffer, buffer_len + 1, 0);
+            if (send_len < 0) {
+              perror("send");
+              close_connection = true;
+              break;
+            }
+          }  // while (1)
+
+          if (close_connection) {
+            close(i);
+            FD_CLR(i, &master_set);
+            if (i == max_sfd) {
+              while (FD_ISSET(max_sfd, &master_set) == false)
+                --max_sfd;
+            }
+          }
+        }
+      }
+    }
+
+  } while (end_flag == false);
+
+  // int cfd = accept(sfd, &client_info, &client_info_len);
+  // if (cfd < 0) {
+  //   perror("accept");
+  //   close(sfd);
+  //   exit(EXIT_FAILURE);
+  // }
+
+  // while (1) {
+  //   memset(buffer, 0, BUFFER_SIZE);
+
+  //   ssize_t recv_len = recv(cfd, buffer, BUFFER_SIZE - 1, 0);
+  //   if (recv_len < 0) {
+  //     perror("recv");
+  //     break;
+  //   }
+  //   printf("recieved: %s\n", buffer);
+
+  //   size_t buffer_len = strlen(buffer);
+  //   buffer[buffer_len] = '$';
+  //   buffer[buffer_len + 1] = '\0';
+
+  //   ssize_t send_len = send(cfd, buffer, buffer_len + 1, 0);
+  //   if (send_len < 0) {
+  //     perror("send");
+  //     break;
+  //   }
+  // }
+  for (int i = 0; i <= max_sfd; ++i) {
+    if (FD_ISSET(i, &master_set))
+      close(i);
   }
-  close(cfd);
-  close(sfd);
   return (0);
 }
